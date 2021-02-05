e1625e51a6e3adadd0f6b539ef7df11952d9a04f
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-05 19:47:06 +0600

    How to apply bind

--- callapply.js
+++ callapply.js
@@ -13,6 +13,32 @@ const normalPerson = {
 }
 
 // console.log(normalPerson.firstName);
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(3000);
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(150);
+// normalPerson.chargeBill(3000);
+// console.log(normalPerson.salary);
+
+
+
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'xyz',
+    salary: 20000,
+    
+}
+
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'xyz',
+    salary: 30000,
+
+}
+// normalPerson.chargeBill();
+const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+heroBillCharge(2000);
+console.log("Her Person Salary     : "+ heroPerson.salary);
+
+const friendlyBillCharge = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyBillCharge(2500);
+console.log("Friendly Person salary: "+ friendlyPerson.salary);
+
+console.log("Normal Person Salary  : "+ normalPerson.salary);
