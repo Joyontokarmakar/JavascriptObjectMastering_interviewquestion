4b63647da0c9f29e80324312a942666ad08c6f0d
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-05 23:36:06 +0600

    How to use setTimeout

--- timeout.js
+++ timeout.js
@@ -3,6 +3,10 @@ console.log(1111);
 console.log(2222);
 setTimeout(function(){
     console.log(2525);
-}, 1000);
+}, 1000); //it will execute atleast 1000 millisecond later.
 console.log(3333);
 console.log(4444);
+
+setInterval(function() {
+    console.log("setinterval")
+}, 1000); //it will execute after each 1000 millisecond
